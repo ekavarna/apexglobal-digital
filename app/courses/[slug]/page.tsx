774877fafe82095265599ec651b7f3d58821b14f
ekavarna-snehasis <121296308+ekavarna-snehasis@users.noreply.github.com>
2025-09-01 22:05:47 +0530

    overview section

---
+++ app/courses/[slug]/page.tsx
@@ -0,0 +1,153 @@
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import CourseNav from "@/components/courseNav";
+
+const cardData = [
+  {
+    imageSrc: "/Low-Carbon-Xposium-page-thumbnail.png",
+    imageAlt: "Low Carbon Xposium Thumbnail",
+    title: "Low-Carbon Strategies for Achieving Net-Zero Targets",
+    enrolledUser: "22",
+    description:
+      "Whether you’re a sustainability officer, a compliance manager, or a business leader, this webinar will provide you with the tools, insights, and strategies necessary to take tangible steps toward net-zero today.",
+    link: "/courses/low-carbon-strategies-for-achieving-net-zero-targets/",
+  },
+  {
+    imageSrc: "/Low-Carbon-Xposium-page-thumbnail.png",
+    imageAlt: "Sustainable Supply Chain Thumbnail",
+    title: "Building a Sustainable Supply Chain",
+    enrolledUser: "10",
+    description:
+      "Learn how to integrate sustainable practices into your supply chain to reduce environmental impact and improve efficiency.",
+    link: "/courses/sustainable-supply-chain/",
+  },
+  {
+    imageSrc: "/Low-Carbon-Xposium-page-thumbnail.png",
+    imageAlt: "Renewable Energy Webinar Thumbnail",
+    title: "Transitioning to Renewable Energy Sources",
+    enrolledUser: "15",
+    description:
+      "Explore the latest trends and technologies in renewable energy to power your business sustainably.",
+    link: "/courses/renewable-energy-sources/",
+  },
+];
+
+const courseData = {
+  imageSrc: "/bg.jpg",
+  imageAlt: "Low Carbon Xposium Thumbnail",
+  courseName: "Course Name",
+  courseSubtitle: "Course Subtitle",
+};
+
+const page = () => {
+  return (
+    <div>
+      <section
+        className="relative flex items-center justify-center min-h-[500px] bg-gray-100 py-12 px-4"
+        style={{
+          backgroundImage: `url('${courseData.imageSrc}')`,
+        }}
+      >
+        {/* Background Overlay */}
+        <div className="absolute inset-0 bg-apex-blue-dark opacity-80 z-0"></div>
+
+        {/* Container */}
+        <div className="relative container mx-auto max-w-7xl px-4 z-10">
+          <div className="flex flex-col items-center text-center">
+            {/* Heading 1 */}
+            <h1 className="text-4xl md:text-5xl font-bold text-white mb-4">
+              {courseData.courseName}
+            </h1>
+
+            {/* Heading 2 */}
+            <h2 className="text-2xl  text-white font-light mb-3 ">
+              {courseData.courseSubtitle}
+            </h2>
+
+            <Link
+              href="/courses"
+              className="mt-4 text-lg inline-block bg-apex-blue-dark uppercase tracking-wide text-white hover:text-white hover:bg-transparent  border-2 border-apex-blue-dark px-8 py-2  rounded-md transition-colors"
+            >
+              Enroll Now
+            </Link>
+          </div>
+        </div>
+      </section>
+
+      <section className=" mx-auto px-4 py-4 sticky top-20 bg-white z-10 shadow-lg">
+        <div className="container mx-auto py-4">
+          <CourseNav />
+        </div>
+      </section>
+
+      {/* overview Section */}
+      <section id="overview" className="py-12 bg-white ">
+        <div className="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 shadow-2xl rounded-lg py-5">
+          {/* Overview Heading */}
+          <p className="text-base font-bold tracking-wide text-white mb-8 absolute left-0 top-0 bg-transparent  bg-gradient-to-r from-apex-green to-apex-blue-light rounded-tl-lg p-3.5">
+            Overview
+          </p>
+
+          {/* Inner Section with Columns */}
+          <div className="grid grid-cols-1 md:grid-cols-[7fr_3fr] gap-12 pt-18">
+            {/* First Column: Text Content */}
+            <div className="space-y-6 ">
+              <h2 className="text-2xl md:text-3xl font-normal leading-snug text-gray-900">
+                Why you should go for BCMLI?
+              </h2>
+              <p className="text-base text-gray-700">
+                A Business Continuity Management System, if not developed and
+                managed properly, can be seen as the proverbial millstone around
+                an organization’s neck. By properly implementing your BCMS, the
+                organization will enjoy the benefits of a management system
+                which has been developed to meet its specific needs.
+              </p>
+            </div>
+
+            {/* Third Column: Image */}
+            <div className="relative">
+              <Image
+                src="/BCMLI.jpg"
+                alt="BCMLI Overview"
+                width={800}
+                height={800}
+                className="object-cover rounded-lg"
+                priority
+              />
+            </div>
+          </div>
+
+          {/* Key Highlights Section */}
+          <div className="mt-12">
+            <h2 className="text-2xl md:text-3xl font-bold text-gray-900 mb-6">
+              Key Highlights
+            </h2>
+            <ul className="list-disc pl-5 space-y-4 text-base text-gray-700">
+              <li className="li-highlight">
+                Designed in partnership with the International Consortium for
+                Organizational Resilience (ICOR), this 3-day training serves as
+                a solid “how to” guide for building a business continuity
+                program for all types of organizations.
+              </li>
+              <li className="li-highlight">
+                Student activities are included throughout the course and are
+                designed as knowledge checks to reinforce lesson materials.
+              </li>
+              <li className="li-highlight">
+                Globally – Recognized Certification Program
+              </li>
+              <li className="li-highlight">
+                BCMLI courses are conducted by certified ICOR instructors who
+                will help you master a structured curriculum supplemented with
+                industry relevant examples.
+              </li>
+            </ul>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default page;
---
+++ components/courseNav.tsx
@@ -0,0 +1,31 @@
+import Link from "next/link";
+import { FC } from "react";
+
+const navItems = [
+  { href: "#overview", label: "Overview" },
+  { href: "#training", label: "Training Options" },
+  { href: "#pre-requisites", label: "Pre-requisites" },
+  { href: "#curriculum", label: "Curriculum" },
+  { href: "#attend", label: "Who should attend" },
+  { href: "#reviews", label: "Reviews" },
+  { href: "#faq", label: "FAQs" },
+];
+
+const courseNav: FC = () => {
+  return (
+    <ul className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-6 list-none">
+      {navItems.map((item, index) => (
+        <li key={index} className="menu-item">
+          <Link
+            href={item.href}
+            className="text-apex-text text-lg pb-3 font-medium hover:text-apex-blue-dark hover:underline decoration-2 hover:underline-offset-4  transition-colors duration-200 "
+          >
+            {item.label}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default courseNav;
